3d0f1653e51080eba1689c0bf8de98c7530f236b
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-03-01 16:31:18 +0100

    sto provando a confrontare i valori di una  array, faccio vari tentativi ma ancora non trovo la soluzione

--- script/script.js
+++ script/script.js
@@ -2,17 +2,52 @@
 let arrWordGrow= [];
 let arrWordDecreasing = [];
 let word = prompt("Insert word");
-// word.length
-for (let i=0; i < word.length; i++){
-arrWordGrow.push(word[i]);
-// console.log(word[i]);
+palindromFunction(word);
+// for (let i=0; i < word.length; i++){
+// arrWordGrow.push(word[i]);
+
+// }
+// console.log (arrWordGrow);
+
+// for (let i=word.length; i >0 ; i--) {
+//     arrWordDecreasing.push(word[i-1]);
+// }
+// console.log (arrWordDecreasing);
+
+
+
+function palindromFunction(functionWord) {
+    for (let i=0; i < functionWord.length; i++){
+        arrWordGrow.push(functionWord[i]);
+        
+    }    
+    // console.log (arrWordGrow);
+
+    for (let n=functionWord.length; n >0 ; n--) {
+            arrWordDecreasing.push(functionWord[n-1]);
+    }
+    // console.log (arrWordDecreasing);
+
+    for (i=0; i < word.length; i++){
+
+
+        if ((arrWordGrow[i] == arrWordDecreasing[i]) == word.length){
+            console.log ("funziona!");
+        } else{
+            console.log ("non funziona!");
+        }
+
+
+        // control = arrWordGrow[i] == arrWordDecreasing[i];
+        // console.log(control);
+        // if (console = word.length) {
+        //     console.log("ciao");
+        // }
+    // console.log(arrWordGrow[i], arrWordDecreasing[i]);
+    // console.log(arrWordDecreasing[i]);
+    // if ( arrWordGrow[i] == arrWordDecreasing[i])  {
+        // console.log(true)
+    // }
+    }
 
 }
-console.log (arrWordGrow);
-
-for (let n=word.length; n >=1 ; n--) {
-    arrWordDecreasing.push(word[n]);
-    // console.log(word[n]);
-}
-
-console.log (arrWordDecreasing);
