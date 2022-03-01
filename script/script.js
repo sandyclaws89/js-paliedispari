472d31d9af4f9cb7bdbb551ef08d766483ecb297
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-03-01 18:13:30 +0100

    sto impostando il secondo esercizio, funzione completata ma non ho capito come si utilizza il to lower case

--- script/script.js
+++ script/script.js
@@ -1,27 +1,27 @@
 
-let arrWordGrow= [];
-let arrWordDecreasing = [];
-let word = prompt("Insert word");
-palindromFunction(word);
-if (palindromFunction(word)) {
-    console.log ("palindroma")
-} else { console.log ("non palindroma")}
+// let arrWordGrow= [];
+// let arrWordDecreasing = [];
+// let word = prompt("Insert word");
+// palindromFunction(word);
+// if (palindromFunction(word)) {
+//     console.log ("palindroma")
+// } else { console.log ("non palindroma")}
 
-function palindromFunction(functionWord) {
-    for (let i=0; i < functionWord.length; i++){
-        arrWordGrow.push(functionWord[i]);
-    }    
-    for (let n=functionWord.length; n >0 ; n--) {
-            arrWordDecreasing.push(functionWord[n-1]);
-    }
-    for (let i=0; i < word.length; i++){
-        if (arrWordGrow[i] == arrWordDecreasing[i]) {
-            return true;
-        } else { 
-           return false;
-        }
-    }
-}
+// function palindromFunction(functionWord) {
+//     for (let i=0; i < functionWord.length; i++){
+//         arrWordGrow.push(functionWord[i]);
+//     }    
+//     for (let n=functionWord.length; n >0 ; n--) {
+//             arrWordDecreasing.push(functionWord[n-1]);
+//     }
+//     for (let i=0; i < word.length; i++){
+//         if (arrWordGrow[i] == arrWordDecreasing[i]) {
+//             return true;
+//         } else { 
+//            return false;
+//         }
+//     }
+// }
 
 // Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
@@ -41,3 +41,23 @@ function palindromFunction(functionWord) {
 //     VEDO SE E' PARI O DISPARI CON %2
 
 // CON IF VEDO CHI HA VINTO CONTROLLANDO IL MAGGIORE
+
+let oddEven = prompt("Odd or even?");
+NON HO CAPITO COME SI USA TO LOWER CASE
+oddEven = oddEven.toLowerCase;
+console.log(oddEven)
+
+
+// if (oddEven = "Odd"){
+//     console.log ("ok")
+// }
+// let num = prompt ("Insert your number");
+
+// let computerNumber = randomFunction(1, 5);
+// console.log(computerNumber)
+
+// function randomFunction(min, max){
+//     return functionNumber = Math.floor(Math.random() * (5)) + 1;
+// }
+
+
